c273ec0ad7d2507f3d9d683a764f64730d7e51ac
Javier <javieralzuu@gmail.com>
2025-09-15 17:33:04 -0300

    first commit

---
+++ bronson-landing-page/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          testred: "#ff0000",
+          DEFAULT: "#2563eb",
+          light: "#60a5fa",
+          dark: "#1e40af",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
