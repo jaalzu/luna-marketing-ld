7d272ab8adebcd2f464a975b67583980348aed8f
Javier <javieralzuu@gmail.com>
2025-09-23 02:30:18 -0300

    header done

--- bronson-landing-page/tailwind.config.js
+++ bronson-landing-page/tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   theme: {
     extend: {
       colors: {
+          black: "#0F172B",
+          grey: "#62748E",
           primary: "#00ffe0",
           secondary: "#1e40af",
-          bgColor: "#f4f8fb"   
+          bgColor: "#f4f8fbff"   
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Inter Display', 'sans-serif'],
       },
     },
   },
